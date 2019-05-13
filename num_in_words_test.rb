92a88558a018693d174fc88300959aad884bf844
Kevin Santos <kevinochieng548@gmail.com>
2019-05-13 15:06:00 +0300

    test conversion of number into words

---
+++ num_in_words_test.rb
@@ -0,0 +1,11 @@
+require 'test/unit'
+require_relative 'num_in_words'
+class NumInWordsTest < Test::Unit::TestCase
+  def test_converting_number_to_words
+    num = Num.new
+    assert_equal(num.number_in_words(1000), 'One thousand')
+    # assert_equal(num.number_in_words(50), 'fifty')
+    assert_equal(num.number_in_words(423), 'Four hundred twenty three')
+    assert_equal(num.number_in_words(400), 'Four hundred')
+  end
+end
