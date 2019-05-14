7e38a11c533b5423a8f07470b481062b144bbf95
Kevin Santos <kevinochieng548@gmail.com>
2019-05-14 10:39:46 +0300

    test two digit numbers

--- num_in_words_test.rb
+++ num_in_words_test.rb
@@ -7,5 +7,7 @@ class NumInWordsTest < Test::Unit::TestCase
     assert_equal(num.number_in_words(423), 'Four Hundred Twenty Three')
     assert_equal(num.number_in_words(400), 'Four Hundred')
     assert_equal(num.number_in_words(5672), 'Five Thousand Six Hundred Seventy Two')
+    assert_equal(num.number_in_words(48), 'Fourty Eight')
+    assert_equal(num.number_in_words(40), 'Fourty')
   end
 end
